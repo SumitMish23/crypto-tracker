e2fb93e9da799825966254baaf2bd2eee3e0c235
SumitMish23 <sumit894sumit@gmail.com>
2022-02-09 13:05:53 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import 'antd/dist/antd.css'
 
-function App() {
+import { Navbar,Exchanges,Homepage,Cryptocurrencies,CryptoDetails,News } from "./components";
+import{Layout,Typography,Space} from "antd"
+import { Switch,Route,Link } from "react-router-dom";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div classname="app">
+        <Navbar></Navbar> 
+      </div>
+
+      <div classname="main">
+      <Layout>
+         <div className="routes">
+           <Switch>
+             <Route exact path="/">
+              < Homepage/> <h1>hi</h1>
+             </Route>
+             <Route exact path="/exchanges">
+              <Exchanges/>
+             </Route>
+             <Route exact path="/cryptocurrencies">
+               <Cryptocurrencies/>
+             </Route>
+             <Route exact path="/crypto/:coinId">
+               <CryptoDetails/>
+             </Route>
+             <Route exact path="/news">
+               <News/>
+             </Route>
+           </Switch>
+         </div>
+      </Layout>
+      
+      <div className="footer">
+      <Typography.Title level={5} style={{color:"white"}}>
+        cryptoverse @2022<br/>
+        All rights reserved
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+        </Space>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
